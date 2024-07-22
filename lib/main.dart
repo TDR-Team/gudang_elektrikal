294164d0c6858a3bcfc6e73565324ce96fd34f95
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-07-22 22:25:29 +0700

    refactor: add screenutilinit
    Co-authored-by: dapraws <darrelprawira26@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
-
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    ResponsiveSizer(builder: (context, orientation, screenType) {
-      return GetMaterialApp(
-        title: "Gudang Elektrikal",
-        initialRoute: AppPages.INITIAL,
-        getPages: AppPages.routes,
-        debugShowCheckedModeBanner: false,
-      );
-    }),
+    const MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return GetMaterialApp(
+          title: "Gudang Elektrikal",
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+        );
+      },
+    );
+  }
 }
