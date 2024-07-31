526b5cb5cd87435cbb1c12d38914f8fe4a8c6f92
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-08-01 03:50:57 +0700

    feat: refactoring bot nav bar
    Co-authored-by: dapraws <darrelprawira26@gmail.com>

--- lib/app/modules/bottom_nav_bar/controllers/bottom_nav_bar_controller.dart
+++ lib/app/modules/bottom_nav_bar/controllers/bottom_nav_bar_controller.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gudang_elektrikal/app/modules/activity/views/activity_view.dart';
 import 'package:gudang_elektrikal/app/modules/borrow_tools/views/borrow_tools_view.dart';
-import 'package:gudang_elektrikal/app/modules/get_components/views/get_components_view.dart';
+import 'package:gudang_elektrikal/app/modules/components/views/components_view.dart';
 import 'package:gudang_elektrikal/app/modules/profile/views/profile_view.dart';
 
 class BottomNavBarController extends GetxController {
@@ -10,7 +10,7 @@ class BottomNavBarController extends GetxController {
 
   final List<Widget> pages = [
     // const HomeView(),
-    const GetComponentsView(),
+    const ComponentsView(),
     const BorrowToolsView(),
     const ActivityView(),
     const ProfileView(),
