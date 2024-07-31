a9f770acb9e65a8a6593fe143bfa2a01b6c4b5c6
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-08-01 03:50:18 +0700

    feat: component model
    Co-authored-by: dapraws <darrelprawira26@gmail.com>

---
+++ lib/app/data/model/component.dart
@@ -0,0 +1,77 @@
+class Component {
+  final int id;
+  final int numberRack;
+  final int numberDrawer;
+  final String name;
+  final String photo;
+  final String description;
+  final int stock;
+
+  
+  Component({
+    required this.id,
+    required this.numberRack,
+    required this.numberDrawer,
+    required this.name,
+    required this.photo,
+    required this.description,
+    required this.stock,
+  });
+}
+
+final listDummyComponents = [
+  Component(
+    id: 1,
+    numberRack: 1,
+    numberDrawer: 1,
+    name: 'Obeng',
+    photo: 'photo',
+    description: 'description',
+    stock: 20,
+  ),
+  Component(
+    id: 2,
+    numberRack: 1,
+    numberDrawer: 2,
+    name: 'Obeng',
+    photo: 'photo',
+    description: 'description',
+    stock: 20,
+  ),
+  Component(
+    id: 3,
+    numberRack: 1,
+    numberDrawer: 3,
+    name: 'Obeng',
+    photo: 'photo',
+    description: 'description',
+    stock: 20,
+  ),
+  Component(
+    id: 4,
+    numberRack: 1,
+    numberDrawer: 4,
+    name: 'Obeng',
+    photo: 'photo',
+    description: 'description',
+    stock: 20,
+  ),
+  Component(
+    id: 5,
+    numberRack: 1,
+    numberDrawer: 5,
+    name: 'Obeng',
+    photo: 'photo',
+    description: 'description',
+    stock: 20,
+  ),
+  Component(
+    id: 6,
+    numberRack: 1,
+    numberDrawer: 6,
+    name: 'Obeng',
+    photo: 'photo',
+    description: 'description',
+    stock: 20,
+  ),
+];
