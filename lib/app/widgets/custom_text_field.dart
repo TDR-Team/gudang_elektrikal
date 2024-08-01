c73992bbb76d4b4ea295ec5b472dc55e75147bc3
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-08-02 05:06:38 +0700

    refactor: add some attribute and edit layout
    Co-authored-by: dapraws <darrelprawira26@gmail.com>

--- lib/app/widgets/custom_text_field.dart
+++ lib/app/widgets/custom_text_field.dart
@@ -1,33 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../common/theme/font.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({
-    super.key,
-    required this.label,
-    this.labelStyle = const TextStyle(
-      fontSize: 14,
-      fontWeight: FontWeight.bold,
-    ),
-    this.isEnabled = true,
-    this.hintText,
-    this.maxLines = 1,
-    this.maxLength,
-    this.onTap,
-    this.textInputType = TextInputType.text,
-    this.isPassword = false,
-    this.isPasswordHide = false,
-    this.isRequired = true,
-    required this.controller,
-    this.onPressedIconPassword,
-    this.isAutoValidate = true,
-    this.inputAction,
-    this.validator,
-    this.onCompleted,
-    this.isPasswordEmpty = false,
-  });
-
-  final String label;
+  final String? label;
   final bool isRequired;
   final TextStyle? labelStyle;
   final bool isEnabled;
@@ -45,48 +22,92 @@ class CustomTextField extends StatelessWidget {
   final String? Function(String?)? validator; // Perubahan tipe data di sini
   final VoidCallback? onCompleted; // Perubahan tipe data di sini
   final bool isPasswordEmpty;
+  final EdgeInsetsGeometry? contentPadding;
+  final int? lengthInput;
+  final TextAlign? textAlign;
+  final FocusNode? focusNode;
+  final TextStyle? textStyle;
+
+  const CustomTextField({
+    super.key,
+    this.label,
+    this.labelStyle,
+    this.isEnabled = true,
+    this.hintText,
+    this.maxLines = 1,
+    this.maxLength,
+    this.onTap,
+    this.textInputType = TextInputType.text,
+    this.isPassword = false,
+    this.isPasswordHide = false,
+    this.isRequired = true,
+    required this.controller,
+    this.onPressedIconPassword,
+    this.isAutoValidate = true,
+    this.inputAction,
+    this.validator,
+    this.onCompleted,
+    this.isPasswordEmpty = false,
+    this.contentPadding,
+    this.lengthInput,
+    this.textAlign = TextAlign.left,
+    this.focusNode,
+    this.textStyle,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        RichText(
-          textAlign: TextAlign.left,
-          text: TextSpan(
-            style: semiBoldText20,
-            children: [
-              TextSpan(
-                text: label,
-                style: labelStyle,
-              ),
-              const WidgetSpan(
-                child: SizedBox(
-                  width: 4,
-                ),
-              ),
-              if (isRequired)
+        Visibility(
+          visible: label != '',
+          child: RichText(
+            textAlign: TextAlign.left,
+            text: TextSpan(
+              style: semiBoldText20,
+              children: [
                 TextSpan(
-                  text: '*',
-                  style: semiBoldText14.copyWith(
-                    color: Colors.red,
+                  text: label,
+                  style: labelStyle ??
+                      TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const WidgetSpan(
+                  child: SizedBox(
+                    width: 4,
                   ),
                 ),
-            ],
+                if (isRequired)
+                  TextSpan(
+                    text: '*',
+                    style: semiBoldText14.copyWith(
+                      color: Colors.red,
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
         const SizedBox(height: 4),
         TextFormField(
+          inputFormatters: [LengthLimitingTextInputFormatter(lengthInput)],
           validator: validator,
           enabled: isEnabled,
           obscureText: isPasswordHide,
           controller: controller,
-          style: const TextStyle(
-            fontSize: 12,
-          ),
+          style: textStyle ??
+              TextStyle(
+                fontSize: 14.sp,
+                color: Colors.black,
+              ),
           keyboardType: textInputType,
           maxLines: maxLines,
           maxLength: maxLength,
+          textAlign: textAlign!,
+          focusNode: focusNode,
           decoration: InputDecoration(
             hintText: hintText,
             border: OutlineInputBorder(
@@ -111,9 +132,9 @@ class CustomTextField extends StatelessWidget {
               ),
               borderRadius: BorderRadius.circular(10),
             ),
-            contentPadding: const EdgeInsets.all(12.0),
+            contentPadding: contentPadding ?? const EdgeInsets.all(12.0),
             // fillColor: Colors.grey[200],
-            fillColor: Colors.transparent,
+            fillColor: Colors.white,
             filled: true,
             suffixIcon: isPassword
                 ? IconButton(
