8dfdbecbc8d1e56e48b90733e1d93d5bd41aebd7
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-08-02 05:04:23 +0700

    feat: add edit components controller
    Co-authored-by: dapraws <darrelprawira26@gmail.com>

---
+++ lib/app/modules/components/controllers/edit_components_controller.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+
+class EditComponentsController extends GetxController {
+  //TODO: Implement EditComponentsController
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+}
