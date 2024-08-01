0f730a5374228c00c9645d8f11164a9bafa4ccfe
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-08-02 05:06:43 +0700

    refactor: add some attribute and edit layout
    Co-authored-by: dapraws <darrelprawira26@gmail.com>

--- lib/app/widgets/dropdown_button.dart
+++ lib/app/widgets/dropdown_button.dart
@@ -3,23 +3,29 @@ import 'package:flutter/material.dart';
 import '../common/theme/font.dart';
 
 class DropDown<T> extends StatelessWidget {
-  final String hintText;
+  final String? hintText;
   final List<T>? listElement;
   final T? selectedItem;
   final ValueChanged<T?>? onChange;
   final String Function(T?)? itemAsString;
   final String? Function(T?)? validator;
   final Widget Function(BuildContext, T?, bool)? itemBuilder;
+  final bool showSearchBox;
+  final Color? fillColor;
+  final BorderSide? borderSide;
 
   const DropDown({
     super.key,
     required this.listElement,
-    required this.hintText,
+    this.hintText,
     this.selectedItem,
     this.onChange,
     this.itemAsString,
     this.validator,
     this.itemBuilder,
+    this.showSearchBox = true,
+    this.fillColor,
+    this.borderSide,
   });
 
   @override
@@ -31,26 +37,24 @@ class DropDown<T> extends StatelessWidget {
       onChanged: onChange,
       validator: validator,
       autoValidateMode: AutovalidateMode.always,
-      
+
       dropdownDecoratorProps: DropDownDecoratorProps(
         baseStyle: regularText12,
         dropdownSearchDecoration: InputDecoration(
-          fillColor: Colors.grey[200], // Warna latar belakang abu-abu
+          fillColor: fillColor ?? Colors.grey[200],
           filled: true,
-        
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide.none,
+            borderSide: borderSide ?? BorderSide.none,
           ),
           hintText: hintText,
         ),
       ),
       popupProps: PopupProps.menu(
-        
         menuProps: MenuProps(
           borderRadius: BorderRadius.circular(16),
         ),
-        showSearchBox: true,
+        showSearchBox: showSearchBox,
         fit: FlexFit.loose,
         searchFieldProps: TextFieldProps(
           style: regularText12,
@@ -86,12 +90,14 @@ class DropDown<T> extends StatelessWidget {
             (context, item, isSelected) {
               return Container(
                 padding: const EdgeInsets.all(12),
-                child: Text(item.toString(), style: regularText12),
+                child: Text(
+                  item.toString(),
+                  style: regularText12,
+                ),
               );
             },
         listViewProps: const ListViewProps(
-          
-          padding: EdgeInsets.only(bottom: 8),
+          padding: EdgeInsets.symmetric(vertical: 8),
         ),
       ),
       // clearButtonProps: const ClearButtonProps(
