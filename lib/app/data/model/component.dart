d7de812bb5d92a02133973f395f7398e9d74749c
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-08-03 21:30:15 +0700

    refactor: add factory fromFirestore and fromMap

--- lib/app/data/model/component.dart
+++ lib/app/data/model/component.dart
@@ -1,77 +1,113 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Component {
-  final int id;
+  final String id;
   final int numberRack;
   final int numberDrawer;
   final String name;
-  final String photo;
+  final String imgUrl;
   final String description;
   final int stock;
+  final String unit;
 
-  
   Component({
     required this.id,
     required this.numberRack,
     required this.numberDrawer,
     required this.name,
-    required this.photo,
+    required this.imgUrl,
     required this.description,
     required this.stock,
+    required this.unit,
   });
+
+  factory Component.fromFirestore(DocumentSnapshot doc) {
+    Map data = doc.data() as Map;
+    return Component(
+      id: doc.id,
+      numberRack: data['numberRack'] ?? 0,
+      numberDrawer: data['numberDrawer'] ?? 0,
+      name: data['name'] ?? '',
+      description: data['description'] ?? '',
+      imgUrl: data['imgUrl'] ?? '',
+      stock: data['stock'] ?? 0,
+      unit: data['unit'] ?? '',
+    );
+  }
+
+  factory Component.fromMap(Map<String, dynamic> data) {
+    return Component(
+      id: data['id'] ?? '',
+      numberRack: data['numberRack'] ?? 0,
+      numberDrawer: data['numberDrawer'] ?? 0,
+      name: data['name'] ?? '',
+      description: data['description'] ?? '',
+      imgUrl: data['imgUrl'] ?? '',
+      stock: data['stock'] ?? 0,
+      unit: data['unit'] ?? '',
+    );
+  }
 }
 
 final listDummyComponents = [
   Component(
-    id: 1,
+    id: "1",
     numberRack: 1,
     numberDrawer: 1,
     name: 'Obeng',
-    photo: 'photo',
+    imgUrl: 'imgUrl',
     description: 'description',
     stock: 20,
+    unit: 'Pcs',
   ),
   Component(
-    id: 2,
+    id: "2",
     numberRack: 1,
     numberDrawer: 2,
     name: 'Obeng',
-    photo: 'photo',
+    imgUrl: 'imgUrl',
     description: 'description',
     stock: 20,
+    unit: 'Pcs',
   ),
   Component(
-    id: 3,
+    id: "3",
     numberRack: 1,
     numberDrawer: 3,
     name: 'Obeng',
-    photo: 'photo',
+    imgUrl: 'imgUrl',
     description: 'description',
     stock: 20,
+    unit: 'Pcs',
   ),
   Component(
-    id: 4,
+    id: "4",
     numberRack: 1,
     numberDrawer: 4,
     name: 'Obeng',
-    photo: 'photo',
+    imgUrl: 'imgUrl',
     description: 'description',
     stock: 20,
+    unit: 'Pcs',
   ),
   Component(
-    id: 5,
+    id: "5",
     numberRack: 1,
     numberDrawer: 5,
     name: 'Obeng',
-    photo: 'photo',
+    imgUrl: 'imgUrl',
     description: 'description',
     stock: 20,
+    unit: 'Pcs',
   ),
   Component(
-    id: 6,
+    id: "6",
     numberRack: 1,
     numberDrawer: 6,
     name: 'Obeng',
-    photo: 'photo',
+    imgUrl: 'imgUrl',
     description: 'description',
     stock: 20,
+    unit: 'Pcs',
   ),
 ];
