8bfa4ee73edfc632f602bfad41082bedb9f73a5b
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-08-10 23:04:35 +0700

    refactor: add searchController

--- lib/app/modules/borrow_tools/controllers/borrow_tools_controller.dart
+++ lib/app/modules/borrow_tools/controllers/borrow_tools_controller.dart
@@ -1,23 +1,11 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class BorrowToolsController extends GetxController {
   //TODO: Implement BorrowToolsController
+  TextEditingController searchController = TextEditingController();
 
   final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
 
   void increment() => count.value++;
 }
