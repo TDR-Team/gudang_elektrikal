bae181d2cfde29b296a72808a722afb70f54850f
dapraws <darrelprawira26@gmail.com>
2024-08-29 17:15:54 +0700

    feat: add tools progress

--- lib/app/modules/tools/bindings/tools_binding.dart
+++ lib/app/modules/tools/bindings/tools_binding.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:gudang_elektrikal/app/modules/tools/controllers/add_tools_controller.dart';
 
 import '../controllers/tools_controller.dart';
 
@@ -7,6 +8,9 @@ class ToolsBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<ToolsController>(
       () => ToolsController(),
+    );
+    Get.lazyPut<AddToolsController>(
+      () => AddToolsController(),
     );
   }
 }
