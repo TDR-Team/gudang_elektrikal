7d1f8ef426582c9cb0117774fca1abf8f17bc1f5
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-09-08 19:52:42 +0700

    refactor: change layout and use controller
    Co-authored-by: dapraws <darrelprawira26@gmail.com>

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,56 +1,61 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:get/get.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:gudang_elektrikal/app/common/theme/font.dart';
 
-class SplashView extends StatelessWidget {
+import 'package:get/get.dart';
+import '../../../common/theme/font.dart';
+import '../controllers/splash_controller.dart';
+
+class SplashView extends GetView<SplashController> {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Delay splash screen for 3 seconds
-    Timer(
-      const Duration(seconds: 3),
-      () => _navigateToNextScreen(), // Navigate after splash
-    );
-
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/img_pln.png'),
+    return GetBuilder<SplashController>(
+      init: SplashController(),
+      builder: (_) {
+        return Scaffold(
+          body: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                    colors: <Color>[
+                      // Color.fromARGB(49, 230, 42, 42),
+                      Color.fromARGB(255, 225, 217, 109),
+                      Color(0xFF28A8E0),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'PUSHARLIS', // Ganti dengan judul aplikasi Anda
-              style: boldText24.copyWith(
-                color: Colors.lightBlue,
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Image.asset(
+                    'assets/images/img_pln.png',
+                    width: MediaQuery.sizeOf(context).width / 2,
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Version 1.0.0',
+                        style: mediumText16,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
-  }
-
-  void _navigateToNextScreen() {
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      // Pengguna sudah login, navigasi ke Dashboard
-      Get.offAllNamed('/dashboard');
-    } else {
-      // Pengguna belum login, navigasi ke LoginView
-      Get.offAllNamed('/login');
-    }
   }
 }
