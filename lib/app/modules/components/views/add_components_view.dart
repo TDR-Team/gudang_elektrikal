855a68f6ee96957329e04ddc8571ce9dbd74706e
dapraws <darrelprawira26@gmail.com>
2024-09-09 18:11:47 +0700

    feat: add components logging

--- lib/app/modules/components/views/add_components_view.dart
+++ lib/app/modules/components/views/add_components_view.dart
@@ -59,12 +59,12 @@ class AddComponentsView extends GetView<AddComponentsController> {
                 const SizedBox(height: 20),
                 CustomTextField(
                   label: 'Nama Komponen',
-                  controller: controller.nameComponent,
+                  controller: controller.nameController,
                 ),
                 const SizedBox(height: 20),
                 CustomTextField(
                   label: 'Deskripsi',
-                  controller: controller.descriptionComponent,
+                  controller: controller.descriptionController,
                   textInputType: TextInputType.multiline,
                   maxLines: 4,
                   maxLength: 100,
