73f2eeac9a09e2500ea1de6ddddecc7156b9367a
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-09-10 09:28:06 +0700

    refactor: delete comment

--- lib/app/modules/components/views/edit_components_view.dart
+++ lib/app/modules/components/views/edit_components_view.dart
@@ -21,7 +21,7 @@ class EditComponentsView extends GetView<EditComponentsController> {
     Get.lazyPut(() => EditComponentsController());
 
     final controller = Get.find<EditComponentsController>();
-    controller.setRackAndLevel();
+    controller.setRackAndLevel(); // Set the rack and level
 
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
@@ -52,31 +52,6 @@ class EditComponentsView extends GetView<EditComponentsController> {
                       imageFileController: controller.imageFileController.value,
                       networkImage: controller.networkImage.value,
                       onPickImage: controller.onPickImage,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'Nama Komponen',
-                  controller: controller.nameController,
-                ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'Deskripsi',
-                  controller: controller.descriptionController,
-                  textInputType: TextInputType.multiline,
-                  maxLines: 4,
-                  maxLength: 100,
-                  isRequired: false,
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        'Stok',
-                        style: semiBoldText16,
-                      ),
                     ),
                   ),
                   const SizedBox(height: 20),
