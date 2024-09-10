b27657dfb63021e29766be548885c2f9378b7276
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-09-10 09:21:02 +0700

    refactor: close keyboard when clicked outside textfield

--- lib/app/modules/components/views/add_components_view.dart
+++ lib/app/modules/components/views/add_components_view.dart
@@ -24,159 +24,162 @@ class AddComponentsView extends GetView<AddComponentsController> {
     final controller = Get.find<AddComponentsController>();
     controller.setRackAndLevel(); // Set the rack and level
 
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          padding: const EdgeInsets.all(16),
-          onPressed: () {
-            FocusScope.of(context).unfocus();
-            Get.back();
-          },
-          icon: Icon(
-            Icons.arrow_back_ios_new_rounded,
-            size: 24.sp,
-            color: Colors.black,
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            padding: const EdgeInsets.all(16),
+            onPressed: () {
+              FocusScope.of(context).unfocus();
+              Get.back();
+            },
+            icon: Icon(
+              Icons.arrow_back_ios_new_rounded,
+              size: 24.sp,
+              color: Colors.black,
+            ),
           ),
+          title: const Text('Tambah Komponen'),
+          centerTitle: false,
         ),
-        title: const Text('Tambah Komponen'),
-        centerTitle: false,
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Obx(
-                  () => _buildImagePicker(
-                    isLoadingImage: controller.isLoadingImage.value,
-                    imageFileController: controller.imageFileController.value,
-                    networkImage: controller.networkImage.value,
-                    onPickImage: controller.onPickImage,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Obx(
+                    () => _buildImagePicker(
+                      isLoadingImage: controller.isLoadingImage.value,
+                      imageFileController: controller.imageFileController.value,
+                      networkImage: controller.networkImage.value,
+                      onPickImage: controller.onPickImage,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'Nama Komponen',
-                  controller: controller.nameController,
-                ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'Deskripsi',
-                  controller: controller.descriptionController,
-                  textInputType: TextInputType.multiline,
-                  maxLines: 4,
-                  maxLength: 100,
-                  isRequired: false,
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        'Stok',
-                        style: semiBoldText16,
-                      ),
-                    ),
-                    Flexible(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: kColorScheme.primary,
-                          borderRadius: BorderRadius.circular(10),
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    label: 'Nama Komponen',
+                    controller: controller.nameController,
+                  ),
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    label: 'Deskripsi',
+                    controller: controller.descriptionController,
+                    textInputType: TextInputType.multiline,
+                    maxLines: 4,
+                    maxLength: 100,
+                    isRequired: false,
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          'Stok',
+                          style: semiBoldText16,
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            IconButton(
-                              onPressed: controller.decrement,
-                              icon: Icon(
-                                Icons.remove,
-                                color: kColorScheme.surface,
-                                size: 24.sp,
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.only(
-                                bottom: 4,
-                              ),
-                              width: 50.w,
-                              child: CustomTextField(
-                                label: '',
-                                controller: controller.stockController,
-                                textAlign: TextAlign.center,
-                                lengthInput: 3,
-                                contentPadding: const EdgeInsets.symmetric(
-                                  vertical: 10,
+                      ),
+                      Flexible(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: kColorScheme.primary,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: controller.decrement,
+                                icon: Icon(
+                                  Icons.remove,
+                                  color: kColorScheme.surface,
+                                  size: 24.sp,
                                 ),
-                                textInputType: TextInputType.number,
-                                focusNode: controller.stockFocusNode,
-                                textStyle: semiBoldText16,
                               ),
-                            ),
-                            IconButton(
-                              onPressed: controller.increment,
-                              icon: Icon(
-                                Icons.add,
-                                color: kColorScheme.surface,
-                                size: 24.sp,
+                              Container(
+                                padding: const EdgeInsets.only(
+                                  bottom: 4,
+                                ),
+                                width: 50.w,
+                                child: CustomTextField(
+                                  label: '',
+                                  controller: controller.stockController,
+                                  textAlign: TextAlign.center,
+                                  lengthInput: 3,
+                                  contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 10,
+                                  ),
+                                  textInputType: TextInputType.number,
+                                  focusNode: controller.stockFocusNode,
+                                  textStyle: semiBoldText16,
+                                ),
                               ),
-                            )
-                          ],
+                              IconButton(
+                                onPressed: controller.increment,
+                                icon: Icon(
+                                  Icons.add,
+                                  color: kColorScheme.surface,
+                                  size: 24.sp,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          'Satuan',
+                          style: semiBoldText16,
                         ),
                       ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        'Satuan',
-                        style: semiBoldText16,
+                      _buildDropDown(
+                        context: context,
+                        listRack: controller.listUnit,
+                        rackName: controller.unitName.value,
+                        onChangedRackName: (value) {
+                          FocusScope.of(context).requestFocus(FocusNode());
+                          controller.onChangedRackName(value);
+                        },
                       ),
-                    ),
-                    _buildDropDown(
-                      context: context,
-                      listRack: controller.listUnit,
-                      rackName: controller.unitName.value,
-                      onChangedRackName: (value) {
-                        FocusScope.of(context).requestFocus(FocusNode());
-                        controller.onChangedRackName(value);
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-              ],
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      bottomNavigationBar: Obx(
-        () => Container(
-          height: 50.h,
-          margin: const EdgeInsets.only(
-            left: 20,
-            right: 20,
-            bottom: 25,
-          ),
-          child: CustomElevatedButton(
-            isLoading: controller.isLoadingAddComponent.value,
-            onPressed: () async {
-              await controller.onAddComponentsClicked();
-            },
-            text: 'Tambah',
-            buttonStyle: primary300Button.copyWith(
-              padding: const WidgetStatePropertyAll(
-                EdgeInsets.only(left: 16),
-              ),
-              overlayColor: WidgetStateProperty.all<Color>(
-                const Color.fromARGB(255, 13, 97, 128),
+        bottomNavigationBar: Obx(
+          () => Container(
+            height: 50.h,
+            margin: const EdgeInsets.only(
+              left: 20,
+              right: 20,
+              bottom: 25,
+            ),
+            child: CustomElevatedButton(
+              isLoading: controller.isLoadingAddComponent.value,
+              onPressed: () async {
+                await controller.onAddComponentsClicked();
+              },
+              text: 'Tambah',
+              buttonStyle: primary300Button.copyWith(
+                padding: const WidgetStatePropertyAll(
+                  EdgeInsets.only(left: 16),
+                ),
+                overlayColor: WidgetStateProperty.all<Color>(
+                  const Color.fromARGB(255, 13, 97, 128),
+                ),
               ),
             ),
           ),
