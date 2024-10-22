7dc6e706f2b72c75d95c2fa461e1eb663f762685
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-10-22 13:16:44 +0700

    feat: add error message firebase

---
+++ lib/app/common/helpers/error_message_firebase_helper.dart
@@ -0,0 +1,32 @@
+class ErrorMessageFirebaseHelper {
+  String getMessageFromErrorCode(String errorCode) {
+    switch (errorCode) {
+      case "ERROR_EMAIL_ALREADY_IN_USE":
+      case "account-exists-with-different-credential":
+      case "email-already-in-use":
+        return "Email already used. Go to login page.";
+      case "ERROR_WRONG_PASSWORD":
+      case "wrong-password":
+        return "Wrong email/password combination.";
+      case "ERROR_USER_NOT_FOUND":
+      case "user-not-found":
+        return "No user found with this email.";
+      case "ERROR_USER_DISABLED":
+      case "user-disabled":
+        return "User disabled.";
+      case "ERROR_TOO_MANY_REQUESTS":
+      case "operation-not-allowed":
+        return "Too many requests to log into this account.";
+      case "ERROR_OPERATION_NOT_ALLOWED":
+        return "Server error, please try again later.";
+      case "channel-error":
+        return "Belum Mengisi";
+      case "invalid-email":
+        return "Email anda invalid";
+      case "invalid-credential":
+        return "Email atau Password anda salah";
+      default:
+        return "Gagal Masuk. Mohon coba lagi";
+    }
+  }
+}
