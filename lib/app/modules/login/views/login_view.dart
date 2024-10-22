f381e9340b2d2bfcc63eee2bf48f704c6849d5c7
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-10-22 13:18:25 +0700

    refactor: change layout and comment header for temp

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
 import 'package:get/get.dart';
@@ -23,146 +24,156 @@ class LoginView extends GetView<LoginController> {
             physics: const ClampingScrollPhysics(),
             keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
             reverse: true,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 70),
-                  Container(
-                    alignment: Alignment.center,
-                    // color: Colors.amber,
-                    width: double.infinity,
-                    child: Container(
-                      child: SvgPicture.asset(
-                        'assets/images/img_login.svg',
-                        // color: Colors.blue,
-                        semanticsLabel: 'My SVG Picture',
-                        height: 200,
-                        fit: BoxFit.fill,
+            child: Column(
+              children: [
+                // Stack(
+                //   clipBehavior: Clip.none,
+                //   alignment: Alignment.center,
+                //   children: [
+                //     Container(
+                //       height: 200.h,
+                //       decoration: BoxDecoration(
+                //         color: AppColors.primaryColors[0],
+                //         borderRadius: const BorderRadius.only(
+                //           bottomRight: Radius.circular(50),
+                //         ),
+                //       ),
+                //     ),
+                //     Image.asset(
+                //       'assets/images/img_pln.png',
+                //       fit: BoxFit.cover,
+                //     ).marginAll(20),
+                //   ],
+                // ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // const SizedBox(height: 70),
+                      Text(
+                        'Selamat Datang!',
+                        style: semiBoldText24,
                       ),
-                    ),
-                  ),
-                  Text(
-                    'Selamat Datang!',
-                    style: semiBoldText24,
-                  ),
-                  const SizedBox(height: 25),
-                  CustomTextField(
-                    label: 'Email',
-                    hintText: 'Masukkan email',
-                    controller: controller.emailController,
-                    textInputType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(height: 16),
-                  CustomTextField(
-                    label: 'Kata Sandi',
-                    hintText: 'Masukkan kata sandi',
-                    controller: controller.passwordController,
-                    isPassword: true,
-                    isPasswordHide: controller.isPasswordHide,
-                    onPressedIconPassword: controller.onPressedIconPassword,
-                    // validator: validatorPassword,
-                  ),
-                  const SizedBox(height: 4),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Forget your password?',
-                        style: mediumText12,
+                      const SizedBox(height: 25),
+                      CustomTextField(
+                        label: 'Email',
+                        hintText: 'Masukkan email',
+                        controller: controller.emailController,
+                        textInputType: TextInputType.emailAddress,
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  InkWell(
-                    onTap: () => controller.onLoginClicked(),
-                    child: Container(
-                      height: 50,
-                      width: double.infinity,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: kColorScheme.secondary,
+                      const SizedBox(height: 16),
+                      CustomTextField(
+                        label: 'Kata Sandi',
+                        hintText: 'Masukkan kata sandi',
+                        controller: controller.passwordController,
+                        isPassword: true,
+                        isPasswordHide: controller.isPasswordHide,
+                        onPressedIconPassword: controller.onPressedIconPassword,
+                        // validator: validatorPassword,
+                      ),
+                      const SizedBox(height: 4),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Lupa password?',
+                            style: mediumText12,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      InkWell(
+                        onTap: () => controller.signIn(),
                         borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          width: double.infinity,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: kColorScheme.secondary,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: controller.isLoading
+                              ? const Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: 3,
+                                  ),
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                  ),
+                                )
+                              : Text(
+                                  'Masuk',
+                                  style: mediumText18.copyWith(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                        ),
                       ),
-                      child: controller.isLoading
-                          ? const Padding(
-                              padding: EdgeInsets.symmetric(
-                                vertical: 3,
-                              ),
-                              child: CircularProgressIndicator(
-                                color: Colors.white,
-                              ),
-                            )
-                          : Text(
-                              'Masuk',
-                              style: mediumText18.copyWith(
-                                color: Colors.black,
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: AppColors.neutralColors[3],
+                              thickness: 1,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              'atau Masuk dengan',
+                              style: regularText10,
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: AppColors.neutralColors[3],
+                              thickness: 1,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        child: CustomElevatedButton(
+                          onPressed: () {
+                            controller.signInWithGoogle();
+                          },
+                          text: 'dengan Google',
+                          textStyle: mediumText14,
+                          buttonStyle: ButtonStyle(
+                            elevation: const WidgetStatePropertyAll(0),
+                            shadowColor: const WidgetStatePropertyAll(
+                                Colors.transparent),
+                            padding: const WidgetStatePropertyAll(
+                              EdgeInsets.symmetric(
+                                vertical: 12,
                               ),
                             ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          color: AppColors.neutralColors[3],
-                          thickness: 1,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          'or login with',
-                          style: regularText10,
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          color: AppColors.neutralColors[3],
-                          thickness: 1,
+                            backgroundColor: const WidgetStatePropertyAll(
+                              Colors.white,
+                            ),
+                            shape: WidgetStatePropertyAll(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                side: BorderSide(
+                                  color: AppColors.neutralColors[3],
+                                ),
+                              ),
+                            ),
+                          ),
+                          icon: SvgPicture.asset('assets/icons/ic_google.svg'),
                         ),
                       ),
                     ],
                   ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    child: CustomElevatedButton(
-                      onPressed: () {
-                        controller.signInWithGoogle();
-                      },
-                      text: 'with Google',
-                      textStyle: mediumText14,
-                      buttonStyle: ButtonStyle(
-                        elevation: const WidgetStatePropertyAll(0),
-                        shadowColor:
-                            const WidgetStatePropertyAll(Colors.transparent),
-                        padding: const WidgetStatePropertyAll(
-                          EdgeInsets.symmetric(
-                            vertical: 16,
-                          ),
-                        ),
-                        backgroundColor: const WidgetStatePropertyAll(
-                          Colors.white,
-                        ),
-                        shape: WidgetStatePropertyAll(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            side: BorderSide(
-                              color: AppColors.neutralColors[3],
-                            ),
-                          ),
-                        ),
-                      ),
-                      icon: SvgPicture.asset('assets/icons/ic_google.svg'),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           bottomNavigationBar: Padding(
