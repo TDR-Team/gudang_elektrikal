8731566a2a34ff3adcd62f98443cd74ac9515e74
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-10-22 13:16:29 +0700

    refactor: move binding to dashboard

--- lib/app/modules/profile/bindings/profile_binding.dart
+++ lib/app/modules/profile/bindings/profile_binding.dart
@@ -1,14 +1,10 @@
 import 'package:get/get.dart';
 import 'package:gudang_elektrikal/app/modules/profile/controllers/edit_profile_controller.dart';
 
-import '../controllers/profile_controller.dart';
 
 class ProfileBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<ProfileController>(
-      () => ProfileController(),
-    );
     Get.lazyPut<EditProfileController>(
       () => EditProfileController(),
     );
