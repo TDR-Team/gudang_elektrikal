d024458751ad5cbb0995318f041287cbca7b30cb
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-10-26 15:25:58 +0700

    refactor: change logic o sendPassowrdResetLink

--- lib/app/modules/login/controllers/forgot_password_controller.dart
+++ lib/app/modules/login/controllers/forgot_password_controller.dart
@@ -1,16 +1,15 @@
-import 'dart:math';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:gudang_elektrikal/app/utils/logging.dart';
 import 'package:gudang_elektrikal/app/widgets/custom_snackbar.dart';
 
 class ForgotPasswordController extends GetxController {
   User? user;
   bool isLoading = false;
-  TextEditingController emailController = TextEditingController();
   final _auth = FirebaseAuth.instance;
+
+  TextEditingController emailController = TextEditingController();
 
   @override
   void onClose() {
@@ -18,25 +17,53 @@ class ForgotPasswordController extends GetxController {
     super.onClose();
   }
 
-  void sendEmailVerificationLink() async {
-    try {
-      await _auth.currentUser?.sendEmailVerification();
-    } catch (e, st) {
-      log.e('Error: $e, location: $st');
-      CustomSnackbar(success: false, title: 'Gagal', message: e.toString());
-    }
-  }
-
-  Future<void> sendPasswordResetLink({required String email}) async {
+  Future<void> sendPasswordResetLink() async {
     isLoading = true;
     update();
     try {
-      await _auth.sendPasswordResetEmail(email: email);
+      final email = emailController.text.trim();
+      if (email.isEmpty) {
+        throw Exception("Email tidak boleh kosong");
+      }
+
+      // Cek keberadaan email di Firestore
+      final userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .where('email', isEqualTo: email)
+          .limit(1)
+          .get();
+
+      if (userDoc.docs.isEmpty) {
+        const CustomSnackbar(
+          success: false,
+          title: 'Gagal',
+          message: 'Email belum terdaftar.',
+        ).showSnackbar();
+        return;
+      }
+      await _auth.setSettings(appVerificationDisabledForTesting: true);
+      await _auth.setLanguageCode('id');
+      await _auth.sendPasswordResetEmail(
+        email: email,
+      );
+
+      const CustomSnackbar(
+        success: true,
+        title: 'Email berhasil di kirim',
+        message: 'Mohon cek email anda',
+      ).showSnackbar();
     } on FirebaseAuthException catch (err) {
-      throw Exception(err.message.toString());
+      CustomSnackbar(
+        success: false,
+        title: 'Gagal',
+        message: err.message.toString(),
+      ).showSnackbar();
     } catch (err) {
-      CustomSnackbar(success: false, title: 'Gagal', message: e.toString());
-      throw Exception(err.toString());
+      CustomSnackbar(
+        success: false,
+        title: 'Gagal',
+        message: err.toString(),
+      ).showSnackbar();
     } finally {
       isLoading = false;
       update();
